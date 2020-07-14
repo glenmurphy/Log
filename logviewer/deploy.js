5d29e5b4adf2bc28c5891288bb5dc2ebf36287f0
Glen Murphy <glen@glenmurphy.com>
2020-07-13 21:35:53 -0700

    Change personal deployment mechanism

--- logviewer/deploy.js
+++ logviewer/deploy.js
@@ -5,9 +5,10 @@ var config = [
     host : 'avocado.glenmurphy.com',
     username : 'glen',
     tasks : [
-      { type : 'upload', localFile : 'server.js', remoteFile : '/home/glen/servers/logviewer/server.js'},
       { type : 'upload', localFile : './src/index.html', remoteFile : '/home/glen/servers/logviewer/src/index.html'},
-      { type : 'exec', cmd : 'sudo systemctl restart logviewer'}
+      // for sites running server.js (avocado is running nginx - see /etc/nginx/sites-available/logs.glenmurphy.com)
+      //{ type : 'upload', localFile : 'server.js', remoteFile : '/home/glen/servers/logviewer/server.js'},
+      //{ type : 'exec', cmd : 'sudo systemctl restart logviewer'}
     ]
   }
 ];
